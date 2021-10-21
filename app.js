9f0a85e93f993d1d9048c959dd60698c1c5156a0
Onoriode Dafetta <onoriodedafetta@gmail.com>
2021-10-21 23:24:11 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.display = "block";
+} 
+
+function hideMenu() {
+    navLinks.style.display = "none";
+} 
